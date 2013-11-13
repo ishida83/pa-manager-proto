517e47191037e1bbc440b131624c4738ac8ba2ee
judas1017 <judaszhangly@gmail.com>
2013-11-13 17:46:04 +0800

    update!

---
+++ javascripts/administration.js
@@ -0,0 +1,5 @@
+jQuery(function ($) {
+   $("#user-table").dataTable({
+       "sDom": '<"row"<"col-sm-6 col-xs-5"l><"col-sm-6 col-xs-7"f>>rt<"row"<"col-sm-5 hidden-xs"i><"col-sm-7 col-xs-12 clearfix"p>>'
+   });
+});
