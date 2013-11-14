8033548cfe186e01afb4594865b1f687d12eec78
judas1017 <judaszhangly@gmail.com>
2013-11-14 17:39:11 +0800

    update 20131114

--- javascripts/administration.js
+++ javascripts/administration.js
@@ -1,5 +1,54 @@
+jQuery.noConflict();
+
+function tableCtrl($scope) {
+    $scope.user = {};
+    $scope.user.data = [];
+
+    $scope.role = {};
+    $scope.role.data = [];
+
+    $scope.permission = {};
+    $scope.permission.data = [];
+
+    for (var i = 1; i <= 10; i++) {
+        $scope.user.data.push({
+            "name": "Username" + i,
+            "mail": "Username" + i + "@example.com",
+            "role": (i === 1) ? "Administrator" : "Guest",
+            "status": Math.random() < 0.5 ? "Active" : "Locked",
+            "time": (i === 1) ? "Now": (new Date()).toLocaleString()
+        });
+    }
+};
+
 jQuery(function ($) {
-   $("#user-table").dataTable({
-       "sDom": '<"row"<"col-sm-6 col-xs-5"l><"col-sm-6 col-xs-7"f>>rt<"row"<"col-sm-5 hidden-xs"i><"col-sm-7 col-xs-12 clearfix"p>>'
-   });
+    $('#adminTab a:first').tab('show');
+
+    $("#createUserBtn").on("click", function () {
+        $("#userModal").modal();
+        $("#role-selection")
+            .children("i").removeClass("fa-chevron-down")
+            .end()
+            .next().hide();
+    });
+
+    $("#role-selection").on("click", function () {
+        var sign = $(this).children("i");
+
+        if (!sign.hasClass("fa-chevron-down")) {
+            $(this).children("i").addClass("fa-chevron-down")
+            $(this).next().slideDown();
+        }
+    });
+
+    $("table.table").on("click", "a.btn", function (e) {
+       if ($(this).data("op") === "update") {
+           $("#userModal").modal();
+       } else {
+           $("#confirmModal .modal-body")
+               .html("Are you sure you want to <strong class='text-danger'>delete</strong> this user?");
+           $("#confirmModal").modal();
+
+       }
+    });
 });
